617bb52fe98aa5689d00eb1d7b83a0d705079f6f
neomq <neo.minqi@trentglobal.edu.sg>
2022-03-21 09:35:38 +0000

    added mongodb connection

--- index.js
+++ index.js
@@ -34,10 +34,24 @@ const {
 
 
 // 2. Routes
-app.get('/', function(req,res){
-    res.send("Hello world");
-})
+// app.get('/', function(req,res){
+//     res.send("Hello world");
+// })
 
+async function main(){
+
+    await connect(process.env.MONGO_URI, "diy_home_decor")
+
+    app.get('/', async function (req, res) {
+        const data = await getDB().collection('projects')
+            .find({})
+            .toArray(); // find all documents
+
+        res.send(data);
+    })
+
+}
+main();
 
 // 3. Listen
 app.listen(3000, function(){
